e3a922296034598c3e5ca8b879ba4729bb54dc1b
Martin Ahoto <martinahoto4@gmail.com>
2021-02-26 15:09:49 +0200

    A build up of the menu option for a user.

--- lab 01 project/main.cpp
+++ lab 01 project/main.cpp
@@ -5,10 +5,44 @@
 //  Created by MARTIN on 26/02/2021.
 //
 
-#include <iostream>
+#include<iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+
+
+void menu()
+{
+    cout<<"1) draw a horizontal line:"<<endl;
+    
+    cout<<"2) draw a vertical line:"<<endl;
+    
+    cout<<"3) draw a square:"<<endl;
+    
+    cout<<"4) draw a rectangle"<<endl;
+    
+    cout<<"5) quit"<<endl;
+}
+
+
+int main()
+{
+    int choice,length,symbol;
+    
+    
+    while(choice!=5)
+    {
+        cout<<"bye!"<<endl;
+        
+        menu();
+        cin>>choice;
+        
+        
+        
+    }
+    
+    
     return 0;
+
 }
