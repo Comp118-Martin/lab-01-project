26dfe6f6994efbdca21a344357f14cb3132e8d84
Martin Ahoto <martinahoto4@gmail.com>
2021-02-26 15:05:24 +0200

    Initial Commit

---
+++ lab 01 project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab 01 project
+//
+//  Created by MARTIN on 26/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
