121b56e76956dfa133cd12517f3d746ca52ee1f2
Martin Ahoto <martinahoto4@gmail.com>
2021-02-27 09:37:09 +0200

    i modified the code to make it more distinct.from the initial one.

--- lab 01 project/main.cpp
+++ lab 01 project/main.cpp
@@ -11,7 +11,6 @@ using namespace std;
 
 
 
-
 void menu()
 {
     cout<<"1) draw a horizontal line:"<<endl;
@@ -28,19 +27,64 @@ void menu()
 
 int main()
 {
-    int choice,length,symbol;
+    int choice = 0,length,symbol;
     
     
     while(choice!=5)
     {
-        cout<<"bye!"<<endl;
+        
         
         menu();
+        cout<<"enter your choice:"<<endl;
         cin>>choice;
         
+        //for horizontal line
+        if(choice==1)
+        {
+            cout<<"enter length:"<<endl;
+            cin>>length;
+            
+            cout<<"enter a character here:"<<endl;
+            cin>>symbol;
+            
+        }
         
+        //for vertical line()
+        if(choice==2)
+        {
+            cout<<"enter the length:"<<endl;
+            cin>>length;
+            
+            cout<<"enter a chaaracter here:"<<endl;
+            cin>>symbol;
+        }
         
-    }
+        if(choice==3)
+        {
+            cout<<"enther the length:"<<endl;
+            cin>>length;
+            
+            cout<<"enter the charater:"<<endl;
+            cin>>symbol;
+        }
+        
+        if(choice==4)
+        {
+            cout<<"enter the length:"<<endl;
+            cin>>length;
+            
+            cout<<"enter the character here:"<<endl;
+            cin>>symbol;
+        }
+        
+        if(choice<0)
+        {
+            cout<<"invalid input!"<<endl;
+        }
+        
+        cout<<"bye!"<<endl;
+        
+      }
     
     
     return 0;
