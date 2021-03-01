17969dbd4064dced0fe25d99d949cef378633d32
Martin Ahoto <martinahoto4@gmail.com>
2021-03-01 12:08:57 +0200

    this is a code for lab 1 but then my codes don't seem to run like how i wanted it to run. can you please take a look at it for me.thanks

--- lab 01 project/main.cpp
+++ lab 01 project/main.cpp
@@ -12,13 +12,17 @@ using namespace std;
 
 void menu();
 
+void Draw_Rectangle(const int height, const int length, const char ch);
+
+void Draw_Square(const int size,const char ch);
+
 void Draw_A_Horizontal_line(const int length,const char ch );
 
-void Draw_A_Vertical_line(const int height,const char ch);
+void Draw_A_Vertical_line(const int length,const char ch);
 
 int main()
 {
-    int choice = 0,length = 0,height=0,symbol;
+    int choice = 0,length = 0,height=0, size=0,symbol;
     
     
     while(choice!=5)
@@ -39,36 +43,48 @@ int main()
             
             Draw_A_Horizontal_line(length,symbol);
             
+            
+            
         }
         
         //for vertical line()
         if(choice==2)
         {
             cout<<"enter the height:"<<endl;
-            cin>>height;
+            cin>>length;
             
             cout<<"enter a chaaracter here:"<<endl;
             cin>>symbol;
             
-            Draw_A_Vertical_line(height,symbol);
+            Draw_A_Vertical_line(length,symbol);
         }
         
+        //for sqaure
         if(choice==3)
         {
-            cout<<"enther the height:"<<endl;
-            cin>>height;
+            cout<<"enther the size:"<<endl;
+            cin>>size;
             
             cout<<"enter the charater:"<<endl;
             cin>>symbol;
+            
+            Draw_Square(size,symbol);
+            
         }
         
+        //for rectangle
         if(choice==4)
         {
             cout<<"enter the length:"<<endl;
             cin>>length;
             
+            cout<<"enter height"<<endl;
+            cin>>height;
+            
             cout<<"enter the character here:"<<endl;
             cin>>symbol;
+            
+            Draw_Rectangle(length,height,symbol);
         }
         
         if(choice<0)
@@ -85,12 +101,69 @@ int main()
 }
 
 
-void Draw_A_vertical_line(const int height,const char ch)
+void Draw_Rectangle(const int height,const int length,const char ch)
+{
+    for(int length=1; length<=height; length++)
+        {
+       if(length <= 1)
+       for(int length=1; length<=length; length++)
+                {
+                    cout<< ch;
+                }
+            else if(length<height)
+            {
+                cout<< endl;
+                for(int length2=1; int height2=length; length2++)
+          {
+          if(length2==1 || length2==length)
+                        cout<< ch;
+                    else
+                        cout<< " ";
+                } //end of for  variable width2
+            }// end of else if
+            else
+            {
+                cout<< endl;
+    for(int length3=1; length3<=length; length3++)
+       {
+                    cout<<ch;
+                }//end of for having variable width3
+            }// end of else
+        }// end of first for loop
+}
+
+
+void Draw_Square(const int size,const char ch)
+{
+    for(int a=1;a<=10;a++)
+            {
+                cout<<ch;
+            }
+                cout<<endl;
+    for(int b=1;b<=5;b++)
+        {
+        for(int c=1;c<=1;c++)
+            {
+                    cout<<ch;
+            }
+            for(int d=1;d<=8;d++)
+                  {
+                    cout<<" ";
+                  }
+                    cout<<ch<<endl;
+           }
+    for(int e=1;e<=10;e++)
+     {
+     cout<<ch;
+     }
+}
+
+void Draw_A_vertical_line(const int length,const char ch)
 {
     cout<<"vertical line"<<endl;
     
     
-    for(int i=0; i < height; ++i)
+    for(int i=0; i < length; ++i)
     {
         cout<<ch<<endl;
     }
@@ -102,9 +175,11 @@ void Draw_A_Horizontal_line(const int length,const char ch)
     
     for(int i=0; i<length; ++i)
     {
+        
         cout<<ch;
     }
     cout<<"\n";
+    
 }
 
 void menu()
